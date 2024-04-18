32f0ea296bec108b2c1985abf63e663ec8ce8acd
JaymezS <james.shaojiaxuan@gmail.com>
2024-04-18 13:50:40 -0400

    Added shop item class

--- script/shopItem.js
+++ script/shopItem.js
@@ -1,8 +1,28 @@
 class shopItem {
-  constructor(image, id) {
-    this.image = image
+  constructor(imageSrc, id) {
+    this.imageSrc = imageSrc
     this.id = id
+    this.HTML_CONTAINER;
   }
 
+  createItemHTML() {
+    this.HTML_CONTAINER = document.createElement("div");
+    this.HTML_CONTAINER.setAttribute("class", "shop-item");
+
+    const LABEL = document.createElement("p");
+    LABEL.innerText = this.id;
   
+    const IMAGE = document.createElement("img")
+    IMAGE.src = `../imgs/${this.imageSrc}`; 
+    IMAGE.style.width = "160px";
+    IMAGE.style.height = "90px";
+
+    this.HTML_CONTAINER.appendChild(IMAGE)
+    this.HTML_CONTAINER.appendChild(LABEL)
+  }
+
+  deleteItemHTML() {
+    this.HTML_CONTAINER.remove();
+  }
+
 }
