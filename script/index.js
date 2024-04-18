3434fa2af3a463c65c30436149691b49a382aded
meli1206 <linmelissa168@gmail.com>
2024-04-18 10:32:03 -0400

    Add Custom Timer to New Tasks JS

--- script/index.js
+++ script/index.js
@@ -2,7 +2,6 @@
 const TIMER_HOUR_INPUT = document.getElementById("timer-hour-input");
 const TIMER_MINUTE_INPUT = document.getElementById("timer-minute-input");
 const TIMER_SECOND_INPUT = document.getElementById("timer-second-input");
-const SET_TIMER_BUTTON = document.getElementById("set-timer-button");
 const TIMER_START_BUTTON = document.getElementById("timer-start-button");
 const TIMER_STOP_BUTTON = document.getElementById("timer-stop-button");
 const TIMER_DISPLAY = document.getElementById("timer-display");
@@ -54,21 +53,6 @@ SET_LONG_BREAK_BUTTON.addEventListener("click", () => {
   setTimer(600);
 })
 
-SET_TIMER_BUTTON.addEventListener("click", () => {
-  const TIME_HOUR_INPUT = Number(TIMER_HOUR_INPUT.value);
-  const TIME_MINUTE_INPUT = Number(TIMER_MINUTE_INPUT.value);
-  const TIME_SECOND_INPUT = Number(TIMER_SECOND_INPUT.value);
-
-  TIMER_HOUR_INPUT.value = "";
-  TIMER_MINUTE_INPUT.value = "";
-  TIMER_SECOND_INPUT.value = "";
-
-  if (!isNaN(TIME_HOUR_INPUT) && !isNaN(TIME_MINUTE_INPUT) && !isNaN(TIME_SECOND_INPUT)) {
-    GLOBAL_TIMER.setTime(Number(TIME_HOUR_INPUT * 3600 + TIME_MINUTE_INPUT * 60 + TIME_SECOND_INPUT));
-    GLOBAL_TIMER.displayTime();
-  }
-})
-
 TIMER_START_BUTTON.addEventListener("click", () => {
   GLOBAL_TIMER.runTimer();
 })
@@ -81,7 +65,12 @@ TIMER_STOP_BUTTON.addEventListener("click", () => {
 //Functions
 function addNewTask() {
   const TASK_NAME = TASK_NAME_INPUT.value;
-  const TASK_TIME = Number(TASK_TIME_INPUT.value);
+
+  const TIME_HOUR_INPUT = Number(TIMER_HOUR_INPUT.value);
+  const TIME_MINUTE_INPUT = Number(TIMER_MINUTE_INPUT.value);
+  const TIME_SECOND_INPUT = Number(TIMER_SECOND_INPUT.value);
+
+  const TASK_TIME = Number(TIME_HOUR_INPUT * 3600 + TIME_MINUTE_INPUT * 60 + TIME_SECOND_INPUT);
 
   if (!isNaN(TASK_TIME)) {
     const NEW_TASK = new Task(TASK_NAME, TASK_TIME);
